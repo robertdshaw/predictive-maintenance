35c9a6da5ced23d5b00619b5cd3fa2f442ac1a4d
MissiHansson <missi.hansson@gmail.com>
2024-10-03 14:34:41 +0200

    added fler urls to csv_module

--- notebooks/CSV_module.py
+++ notebooks/CSV_module.py
@@ -53,7 +53,11 @@ class CSVReader:
 
 
 file_urls = [
-    "https://raw.githubusercontent.com/JakobRask/predictive-maintenance/main/data/PdM_errors.csv"
+    "https://raw.githubusercontent.com/JakobRask/predictive-maintenance/main/data/PdM_errors.csv",
+    "https://raw.githubusercontent.com/JakobRask/predictive-maintenance/main/data/PdM_failures.csv",
+    "https://raw.githubusercontent.com/JakobRask/predictive-maintenance/main/data/PdM_machines.csv",
+    "https://raw.githubusercontent.com/JakobRask/predictive-maintenance/main/data/PdM_maint.csv",
+    "https://raw.githubusercontent.com/JakobRask/predictive-maintenance/main/data/PdM_telemetry.csv"
 ]
 
 csv_reader = CSVReader(file_urls, 'csv_reader_log.log')
